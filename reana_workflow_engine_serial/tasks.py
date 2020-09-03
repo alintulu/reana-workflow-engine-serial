08d42c3c0b836f5f3be45d224add925a72e43e8d
alintulu <adelina.eleonora.lintuluoto@cern.ch>
2020-09-03 16:13:10 +0200

    fix typo for max runtime field

--- reana_workflow_engine_serial/tasks.py
+++ reana_workflow_engine_serial/tasks.py
@@ -189,7 +189,7 @@ def run_step(
             unpacked_image=step.get("unpacked_image", False),
             kubernetes_uid=step.get("kubernetes_uid", None),
             voms_proxy=step.get("voms_proxy", False),
-            htcondor_max_runtime=step.get("htcondor_max_runtime", False),
+            htcondor_max_runtime=step.get("htcondor_max_runtime", None),
         )
 
         job_spec_copy = dict(job_spec)
